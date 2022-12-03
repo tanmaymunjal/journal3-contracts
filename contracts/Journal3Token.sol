9d0c4788a61d212776f930e0fd8b3d2568814bfa
sameshl <samesh.lakhotia@gmail.com>
2022-12-04 01:32:54 +0530

    Add Journal3Token contract

---
+++ contracts/Journal3Token.sol
@@ -0,0 +1,26 @@
+
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.3;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract Journal3Token is ERC20 {
+
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        // mint 10 Million tokens for owner
+        _mint(msg.sender, 10000000 * (10 ** 18));
+    }
+
+    mapping(address => uint) public lockTime;
+
+    function requestTokens (address requestor , uint amount) external {
+        
+        require(block.timestamp > lockTime[msg.sender], "lock time has not expired. Please try again later");
+
+        transfer(requestor, amount);
+
+        //updates locktime 5 minutes from now
+        lockTime[msg.sender] = block.timestamp + 5 minutes;
+    }
+}
