1b6ba4114ca032e0d049bcfeb65948e9c9efdf4e
never2average <priyesh.sriv2017@gmail.com>
2022-12-03 15:16:57 +0530

    Setup Commit

--- contracts/KaggleDataOracle.sol
+++ contracts/KaggleDataOracle.sol
@@ -7,6 +7,12 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract KaggleDataOracle is Ownable{
+
+    struct RawData{
+        bytes32 ipfs;
+        bool isActive;
+        address verifiedBy;
+    }
     
     //       JobSeeker.         Notebook        DataPoint       Value.  Staked 
     mapping (address => mapping(string=>mapping(string=>uint))) public userDataOracle;
@@ -14,7 +20,7 @@ contract KaggleDataOracle is Ownable{
             //. validator_address    JobSeeker.         Notebook        DataPoint       Value.  Staked             
     mapping (address=>mapping(address=>mapping(string=>mapping(string=>uint)))) public validatorSplits;
        
-    mapping (address=>bool) public rawDataStore;
+    mapping (address=>RawData) public rawDataStore;
     IERC20 public token;
        
     function getStakeValue(address ad,string memory v2,string memory v3)public view returns(uint){
@@ -27,16 +33,16 @@ contract KaggleDataOracle is Ownable{
         token = IERC20(_tokenAddress);
     }
     
-    function setRawData(address ad, bool set) public {
-        require(msg.sender==owner() || msg.sender == ad,"You can only set for yourself");
-        rawDataStore[ad] = set;
+    function setRawData(address ad,bytes32 ipfs, bool isActive) public {
+        // require(msg.sender==owner() || msg.sender == ad,"You can only set for yourself");
+        rawDataStore[ad] = RawData(ipfs,isActive,msg.sender);
            
     }
        
     function validateDataPoint( address ad,  string memory v2, string memory v3, uint val) public {
-        require(rawDataStore[ad] ==true , "Raw Data not stored!");
+        require(rawDataStore[ad].isActive ==true , "Raw Data not stored!");
         require(val>0,"Value must be greator than 0");
-        token.transfer(msg.sender,address(this),val);
+        token.transferFrom(msg.sender,address(this),val);
         userDataOracle[ad][v2][v3]+=val;
         validatorSplits[msg.sender][ad][v2][v3]+=val;
     }
